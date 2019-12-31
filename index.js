8035b2ef474e02647cea59561dc5931ffb590118
marcelo alves andrade <marceloalvesandrade@MacBook-Pro-de-marcelo.local>
2019-12-31 11:32:33 -0400

    correcao na sintaxe do html

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var user = {};
 function addLine(dataUser) {
     var tr = document.createElement("tr");
 
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
     <tr>
         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
         <td>${dataUser.name}</td>
@@ -18,9 +18,6 @@ function addLine(dataUser) {
             <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
         </td>
     </tr>`;
-
-    document.getElementById("table-users").appendChild(tr);
-
 };
 
 document.getElementById("form-user-create").addEventListener("submit", (event) => {
