4916832efa0888e57c134eecc1f2079c010933e4
marcelo alves andrade <marceloalvesandrade@MacBook-Pro-de-marcelo.local>
2019-12-31 15:35:38 -0400

    aplicado model view controller

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
 
-var fields = document.querySelectorAll('#form-user-create [name]');
+let userController = new UserController("form-user-create", "table-users");
 
-var user = {};
-
-function addLine(dataUser) {
-    var tr = document.createElement("tr");
-
-    document.getElementById("table-users").innerHTML = `
-    <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
-        <td>
-            <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-    </tr>`;
-};
-
-document.getElementById("form-user-create").addEventListener("submit", (event) => {
-    event.preventDefault();
-    fields.forEach(field => {
-        if(field.name == 'gender'){
-            if(field.checked)
-                user[field.name] = field.value;
-        } else {
-            user[field.name] = field.value;
-        }
-    });
-    addLine(user);
-});
