3ed5c476f466567a3a752fc77b0252f56cc7f378
marcelo alves andrade <marceloalvesandrade@MacBook-Pro-de-marcelo.local>
2019-12-31 10:58:50 -0400

    pegando objetos digitados na tela e montando json

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var fields = document.querySelectorAll('#form-user-create [name]');
 
 var user = {};
 
-fields.forEach((field, index) => {
-    if(field.name == 'gender'){
-        if(field.checked)
+document.getElementById("form-user-create").addEventListener("submit", (event) => {
+    event.preventDefault();
+    fields.forEach(field => {
+        if(field.name == 'gender'){
+            if(field.checked)
+                user[field.name] = field.value;
+        } else {
             user[field.name] = field.value;
-    } else {
-        user[field.name] = field.value;
-    }
+        }
+    });
+    console.log(user);
 });
-
-console.log(user);
