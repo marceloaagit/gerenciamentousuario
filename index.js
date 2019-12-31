513d6a903381d411d7cc25ffaf4b2abd5f181607
marcelo alves andrade <marceloalvesandrade@MacBook-Pro-de-marcelo.local>
2019-12-31 10:35:35 -0400

    montando um json com os objetos da tela

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var name = document.querySelector("#exampleInputName");
 
-var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var fields = document.querySelectorAll('#form-user-create [name]');
 
-var birth = document.querySelector("#exampleInputBirth");
-var country = document.querySelector("#exampleInputCountry");
-var email = document.querySelector("#exampleInputEmail");
-var password = document.querySelector("#exampleInputPassword");
-var file = document.querySelector("#exampleInputFile");
-var admin = document.querySelector("#exampleInputAdmin");
+var user = {};
+
+fields.forEach((field, index) => {
+    if(field.name == 'gender'){
+        if(field.checked)
+            user[field.name] = field.value;
+    } else {
+        user[field.name] = field.value;
+    }
+});
+
+console.log(user);
