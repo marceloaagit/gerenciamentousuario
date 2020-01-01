3f97fc1baf81ff5d7cd8288a28e83accc5d73385
marcelo alves andrade <marceloalvesandrade@MacBook-Pro-de-marcelo.local>
2020-01-01 15:42:40 -0400

    correção do upload do arquivo

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -46,7 +46,11 @@ class UserController{
                 reject(e);
             };
     
-            fileReader.readAsDataURL(file);
+            if(file){
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/boxed-bg.jpg');
+            }
 
         });
 
@@ -60,9 +64,14 @@ class UserController{
 
         [...this.formE1.elements].forEach(field => { 
             if(field.name == 'gender'){
-                if(field.checked)
+                if(field.checked){
                     user[field.name] = field.value;
-            } else {
+                }
+            } else if (field.name == 'admin') {
+                user[field.name] = field.checked;
+            } 
+            
+            else {
                 user[field.name] = field.value;
             }
         });
@@ -73,18 +82,19 @@ class UserController{
 
     addLine(dataUser) {
 
-        this.tableE1.innerHTML = `
-        <tr>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
             <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
-            <td>${dataUser.admin}</td>
+            <td>${(dataUser.admin) ? 'Sim': 'Não'}</td>
             <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-            </td>
-        </tr>`;
+            </td>`;
+            this.tableE1.appendChild(tr);
     }
 
     
