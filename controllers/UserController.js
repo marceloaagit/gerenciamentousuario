1cb60b5d3ba1f80eccd9717e65744bb66d94dc33
marcelodrapti <marceloalvesandrade@gmail.com>
2020-01-03 14:26:10 -0400

    implementado o excluir na tela

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -176,7 +176,7 @@ class UserController{
             <td>${Utils.dateFormat(dataUser.register)}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                <button type="button" class="btn btn-danger btn-delete btn-xs btn-flat">Excluir</button>
             </td>`;
 
             this.addEventsTR(tr);
@@ -187,6 +187,13 @@ class UserController{
     }
 
     addEventsTR(tr) {
+
+        tr.querySelector('.btn-delete').addEventListener('click', e => {
+            if(confirm('Deseja realmente excluir?')){
+                tr.remove();
+                this.updateCount();
+            }
+        });
 
         tr.querySelector('.btn-edit').addEventListener('click', e => {
                 
